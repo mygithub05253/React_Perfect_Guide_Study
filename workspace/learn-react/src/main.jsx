e561512af7e7e0dec78c6a48683b7c6e6a890776
mygithub05253 <kik328288@gmail.com>
2025-07-11 15:23:43 +0900

    섹션 2. 프로젝트 이해하기 완료

--- workspace/learn-react/src/main.jsx
+++ workspace/learn-react/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// main.jsx : 애플리케이션의 진입점을 나타내는 파일
+
+import React from 'react'
+
+// ReactDOM : React 애플리케이션을 렌더링하고, 프로젝트의 루트 컴포넌트 지정
+import ReactDOM from 'react-dom/client'
+
+// 애플리케이션의 루트 컴포넌트 import
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// 전역 CSS 스타일을 import하여 애플리케이션에 적용
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // StrictMode : 애플리케이션 내의 잠재적인 문제를 알아내기 위한 도구
+  // 프로덕션 빌드에 영향을 주지 않음
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
